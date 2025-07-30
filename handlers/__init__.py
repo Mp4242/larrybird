6a09aca0f4869f24dfa4dcc5093def29251eaa6a
Larry Bird <bigbob4242@gmail.com>
2025-07-30 18:21:00 +0200

    30.07.2025 - Grok Delete /posts

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,13 @@
-from .main import main_router
+from .database import Base, async_session, init_db
 from .onboarding import onboarding_router
+from .main import main_router
 from .counter import counter_router
+from .replies import replies_router  # Ajout
+from .posts import posts_router
+from .settings import settings_router
 
-__all__ = ["main_router", "onboarding_router"]
+__all__ = [
+    "Base", "async_session", "init_db",
+    "onboarding_router", "main_router", "counter_router",
+    "replies_router", "posts_router", "settings_router"
+]
